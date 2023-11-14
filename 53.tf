ddadca76cf20152208edf53ff4fbb603b096e4ee
hvad <david.hannequin@gmail.com>
2023-11-14 11:58:33 +0100

    Modify 53.

--- 53.tf
+++ 53.tf
@@ -7,14 +7,14 @@ resource "aws_route53_record" "www" {
   zone_id = aws_route53_zone.primary.zone_id
   name    = "www.${var.aws_web_site_name}"
   type    = "A"
-  ttl     = 300
+  ttl     = 86400 # 86400 is normal value and 300 for prepare a change
   records = [aws_eip.my_aws_eip.public_ip]
 }
 
 resource "aws_route53_record" "root" {
   zone_id = aws_route53_zone.primary.zone_id
-  name    = var.aws_web_site_name
+  name    = "${var.aws_web_site_name}"
   type    = "A"
-  ttl     = 300
+  ttl     = 86400 # 86400 is normal value and 300 for prepare a change
   records = [aws_eip.my_aws_eip.public_ip]
 }
