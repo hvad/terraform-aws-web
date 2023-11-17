0b9cb691535ae52edaadc54e7c23076c6ce43978
hvad <david.hannequin@gmail.com>
2023-11-17 10:03:26 +0100

    Add tfvars file and update README.

--- README.md
+++ README.md
@@ -1,32 +1,66 @@
 # Terraform plan to deploy AWS instance and DNS
 
+## Description 
 
-## Route 53 
+This terraform plan deploy Linux server with Apache Httpd and virtual host, create
+record for domain name in existing zone and let's encrypt SSL certificate.
 
-Write route 53 configuration like below in 53.tf file :
+## Requires
+
+Before execute plan you have to create tfvars file with variables below :
+
+- aws_region = "<AWS region to deploy Web server>"
+
+- aws_instance_type = "<Size of instance>"
+
+- aws_instance_ami = "<Amazone machine image Amazone Linux 2023 is a Linux RPM distribution>"
+
+- own_ssh_key = "<Public ssh RSA key without password on private key>"
+
+- own_path_ssh_private_key = "<Path of SSH private key without password>" 
+
+Warning : SSH private key with password are not support by terraform AWS provider.
+
+- security_group_name =  "<Security group name>"
+
+- aws_web_site_name = "<Web site name>" 
+
+- aws_domain_name "<DNS zone with a dot in the end for example : example.net.>"
+
+- own_email = "<Email of contact for let encrypt SSL certificate>"
+
+- own_timezone = "<Time zone of AMI. On Linux Amazone 2023 it is not Paris>"
+
+## How to use 
+
+To check the plan :
 
 ```
-resource "aws_route53_zone" "primary" {
-  name ="${var.aws_web_site_name}." 
-  force_destroy = false
-}
+$ terraform plan -var-file="vars.tfvars"
 ```
 
-Import id zone :
+To deploy :
 
 ```
-$ terraform import aws_route53_zone.primary <zone_id> 
+$ terraform apply -var-file="vars.tfvars"
 ```
 
+To destroy : 
 
-### How to delete any resource added by terraform
+```
+$ terraform destroy
+```
+
+## How to delete any resource added by terraform
 
 List all state :
+
 ```
 $ terraform state list 
 ```
 
 Remove desired resource from state :
+
 ```
 $ terraform state rm <name>
 ```
--- vars.tf
+++ vars.tf
@@ -1,50 +1,49 @@
 variable "aws_region" {
   description = "AWS region"
-  default     = "eu-west-3"
+  type        = string
 }
 
 variable "aws_instance_type" {
   description = "AWS instance type"
-  default     = "t2.micro"
+  type        = string
 }
 
 variable "aws_instance_ami" {
   description = "AMI"
-  default     = "ami-0c95ddc49a2ac351f"
+  type        = string
 }
 
 variable "own_ssh_key" {
   description = "SSH Public key."
-  default     = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQClnHjuM7VbOPCXg2XMudpvRqYKjEpj1FoyiYARPejF86Y4pEULU8DhGkqdK4SpzH8yDSxuKG5sy/kOyS4cV3kHQP6kRpywCdTn0Z2cOdun4kHeMS/BtdL+Qd/xgbhsKVVDtA4e10oqzLmAYDvqh3O/p4kR1nwad2U1txbYNkFhY8OmEyoZNBAo3KDKLrB5aBEPyA9BnfQFUXl69olrp3ypdTMTF9w6ZhbLWRcSJFJM3S3Wyb/FSnCJMjBJXzrMB2L3+C9v99a+qjRV+ZM2ITPclB7iz4hsowFUPL62h5Sb4/wjkgKXCnGoo/RyuXebYMqimLAgLHLlifpTkpnJ23dHc7kaxf2PQtCAykUQyMkBnzEELzecRQvEmdeyPoxzh5lxonPX3JODEtMMyDl8JlrnOj5HcH0PDDPjbGNDlukKuPAoLODDSCrw5YzAbixsJSqof3elhyIzea/ymqmCatIuHOccWfqFWtlZzYpzyv/LwCsvKHHegyMaw8KepHAVpeE= davidhannequin@MacBook-Pro-de-david.local"
+  type        = string
 }
 
 variable "own_path_ssh_private_key" {
   description = "Path to private key"
-  default     = "/Users/davidhannequin/.ssh/ssh-terraform"
+  type        = string
 }
 
 variable "security_group_name" {
   description = "The name of my security group"
   type        = string
-  default     = "terraform-my-aws"
 }
 
 variable "aws_web_site_name" {
   description = "Web site name"
-  default     = "stackthecode.net"
+  type        = string
 }
 
 variable "aws_domain_name" {
   description = "Domaine name"
-  default     = "stackthecode.net."
+  type        = string
 }
 
 variable "own_email" {
   description = "Email"
-  default     = "david.hannequin@gmail.com"
+  type        = string
 }
 
 variable "own_timezone" {
   description = "Time zone"
-  default     = "Europe/Paris"
+  type        = string
 }
